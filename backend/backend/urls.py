167cbd184c24e6eaa10862b7378b487931562283
ChervonnykhTim <chervonnykh.tim@gmail.com>
2026-06-12 13:20:48 +0300

    add home page

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.http import HttpResponse
 from rest_framework import routers
 
 from api import views
@@ -7,7 +8,11 @@ from api import views
 router = routers.DefaultRouter()
 router.register('tasks', views.TaskView, 'task')
 
+def home(request):
+    return HttpResponse("Taski is running")
+
 urlpatterns = [
+    path('', home),
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
 ]
